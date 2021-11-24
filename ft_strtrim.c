99faaafb6aaefb3ed9615e5114cf0d966cdc9e41
Camille Barbit <barbitcamille@gmail.com>
2021-11-24 19:06:43 +0100

    new version

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:43:48 by camillebarb       #+#    #+#             */
-/*   Updated: 2021/11/24 11:43:49 by camillebarb      ###   ########.fr       */
+/*   Updated: 2021/11/24 15:44:21 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 char    *ft_strtrim(const char *s1, const char *set)
 {
     char    *s1bis;
-    int  i;
-    int  j;
+    int     i;
+    int     j;
 
     i = 0;
     if (!s1[i] || !set[i])
@@ -26,6 +26,6 @@ char    *ft_strtrim(const char *s1, const char *set)
     j = ft_strlen(s1) - 1;
     while (j > 0 && ft_strchr(set, s1[j]))
         j--;
-    s1bis = ft_substr((char *)s1, i, (j - i + 1));
+    s1bis = ft_substr(s1, i, (j - i + 1));
     return (s1bis);
 }
