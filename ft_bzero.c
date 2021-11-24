21b4f88a6ddeab5a6ae8a7e308a3f78ecbee2c22
Camille Barbit <barbitcamille@gmail.com>
2021-11-24 09:55:15 +0100

    new version

--- ft_bzero.c
+++ ft_bzero.c
@@ -12,4 +12,18 @@ void	ft_bzero(void *s, size_t n)
 		b[i] = '0';
 		i++;
 	}
+	return (b);
+}
+
+int	main(void)
+{
+	size_t	len;
+	char	s[]= "BONJOUR MON SOLEIL";
+	int	c;
+
+	c = 45;
+	len = 5;
+	printf("%s\n", s);
+	ft_memset(s + 1, c, len);
+	printf("%s\n", s);
 }
