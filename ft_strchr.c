168b00f1af22beee5554a4836b791c398f9babd9
Camille Barbit <barbitcamille@gmail.com>
2021-11-24 19:03:43 +0100

    new version

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:42:11 by camillebarb       #+#    #+#             */
-/*   Updated: 2021/11/24 11:42:13 by camillebarb      ###   ########.fr       */
+/*   Updated: 2021/11/24 13:56:09 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strchr(const char *string, int c)
 	i = 0;
 	while (string[i])
 	{
-		if (string[i] == (char)c)
+		if (string[i] == c)
 			return ((char *)&string[i]);
 		i++;
 	}
