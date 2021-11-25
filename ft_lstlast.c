89a73af5fd23c4317a74d009dc92fe30877ef749
Camille Barbit <barbitcamille@gmail.com>
2021-11-25 09:44:53 +0100

    first version

---
+++ ft_lstlast.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 09:37:19 by camillebarb       #+#    #+#             */
+/*   Updated: 2021/11/25 09:44:00 by camillebarb      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list  *ft_lstlast(t_list *lst)
+{
+    if (!lst)
+        return NULL;
+    while (*lst && lst->next != NULL)
+        lst = lst->next;
+    return (lst);
+}
