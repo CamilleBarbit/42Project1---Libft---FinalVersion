15d1e3892e99b3fac439a53a8691df9f1e21a322
Camille Barbit <barbitcamille@gmail.com>
2021-11-25 12:55:10 +0100

    new version

--- libft.h
+++ libft.h
@@ -7,9 +7,9 @@
 
 typedef struct s_list
 {
-void *content;
-struct s_list *next;
-} t_list
+    void    *content;
+    struct s_list   *next;
+}   t_list;
 
 char    *ft_itoa(int n);
 char    **ft_split(const char *s, char c);
@@ -46,5 +46,11 @@ void    ft_putendl_fd(char *s, int fd);
 void    ft_putnbr_fd(int n, int fd);
 void    ft_putstr_fd(char *s, int fd);
 void    ft_striteri(char *s, void (*f)(unsigned int, char*));
+void    ft_lstadd_front(t_list **alst, t_list *new);
+void    ft_lstadd_back(t_list **alst, t_list *new);
+int     ft_lstsize(t_list *lst);
+t_list  *ft_lstnew(void *content);
+t_list  *ft_lstlast(t_list *lst);
+
 
 #endif
