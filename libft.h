92e4d9fcad275ac5873de0fad099999562329323
Camille Barbit <barbitcamille@gmail.com>
2021-11-26 13:40:13 +0100

    first version

--- libft.h
+++ libft.h
@@ -49,6 +49,8 @@ void    ft_putstr_fd(char *s, int fd);
 void    ft_striteri(char *s, void (*f)(unsigned int, char*));
 void    ft_lstadd_front(t_list **alst, t_list *new);
 void    ft_lstadd_back(t_list **alst, t_list *new);
+void    ft_lstdelone(t_list *lst, void (*del)(void*));
+void    ft_lstclear(t_list **lst, void (*del)(void*));
 int     ft_lstsize(t_list *lst);
 t_list  *ft_lstnew(void *content);
 t_list  *ft_lstlast(t_list *lst);
