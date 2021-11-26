79edfdb2bf9872bb7143ba979683c8e5f4704df6
Camille Barbit <barbitcamille@gmail.com>
2021-11-26 11:12:45 +0100

    new version

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:40:02 by camillebarb       #+#    #+#             */
-/*   Updated: 2021/11/25 13:16:08 by camillebarb      ###   ########.fr       */
+/*   Updated: 2021/11/26 09:44:06 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t			i;
 	unsigned char	*d;
-	const char		*s;
+	unsigned char	*s;
 
 	d = (unsigned char *)dest;
-	s = (const char *)src;
+	s = (unsigned char *)src;
 	i = 0;
+	if (!dest || !src)
+		return (NULL);
 	while (i < n)
 	{
 		d[i] = s[i];
