453f93ff69f8991c4d5d13b29fc8069722d3ab81
Camille Barbit <barbitcamille@gmail.com>
2021-11-26 13:37:40 +0100

    new version

---
+++ ft_lstdelone.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 13:13:55 by camillebarb       #+#    #+#             */
+/*   Updated: 2021/11/26 13:37:27 by camillebarb      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+    (*del)(lst->content);
+    free(lst->content);
+}
