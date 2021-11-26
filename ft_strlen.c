f0e7cd0a049becc5f1ec5316b01d92edaa1bc19d
Camille Barbit <barbitcamille@gmail.com>
2021-11-26 11:15:06 +0100

    new version

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:43:03 by camillebarb       #+#    #+#             */
-/*   Updated: 2021/11/24 11:43:04 by camillebarb      ###   ########.fr       */
+/*   Updated: 2021/11/26 09:30:24 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strlen(const char *str)
+int	ft_strlen(char const *str)
 {
 	int	i;
 
