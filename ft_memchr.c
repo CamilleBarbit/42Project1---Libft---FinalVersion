cde41df0282d489f087a40acbe60cf35981468cc
Camille Barbit <barbitcamille@gmail.com>
2021-11-26 12:11:46 +0100

    new version

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:39:46 by camillebarb       #+#    #+#             */
-/*   Updated: 2021/11/24 11:39:47 by camillebarb      ###   ########.fr       */
+/*   Updated: 2021/11/26 12:08:09 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const char		*sub;
-	unsigned char	b;
-	size_t	i;
+	unsigned char	*sub;
+	size_t			i;
 	
-	sub = (const char *)s;
-	b = (unsigned char)c;
+	sub = (unsigned char *)s;
 	i = 0;
 	while (sub[i] && i < n)
 	{
-		if (sub[i] == b)
-			return ((char *)&sub[i]);
+		if (sub[i] == (unsigned char)c)
+			return ((unsigned char *)&sub[i]);
 		i++;
 	}
 	return (NULL);
