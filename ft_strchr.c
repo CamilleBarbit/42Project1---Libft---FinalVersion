5ccfa7c1c524083fc464f0d3d6c2584aaf251a4e
Camille Barbit <barbitcamille@gmail.com>
2021-11-26 12:12:26 +0100

    new version

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,13 +6,13 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:42:11 by camillebarb       #+#    #+#             */
-/*   Updated: 2021/11/24 13:56:09 by camillebarb      ###   ########.fr       */
+/*   Updated: 2021/11/26 11:49:49 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *string, int c)
+char	*ft_strchr(char const *string, int c)
 {
 	int	i;
 
