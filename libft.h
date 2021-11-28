f13b36e665aa2bd71f4bcffebcc85ff54bd9e1ff
Camille Barbit <barbitcamille@gmail.com>
2021-11-28 14:35:04 +0100

    last version

--- libft.h
+++ libft.h
@@ -55,6 +55,6 @@ void    ft_lstiter(t_list *lst, void (*f)(void *));
 int     ft_lstsize(t_list *lst);
 t_list  *ft_lstnew(void *content);
 t_list  *ft_lstlast(t_list *lst);
-
+t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *)).
 
 #endif
