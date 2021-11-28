7d23e7c9faaed1cc7bc9fbd6482a4e540d61314c
Camille Barbit <barbitcamille@gmail.com>
2021-11-28 12:31:16 +0100

    first version:need to check

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,10 +6,21 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 13:39:28 by camillebarb       #+#    #+#             */
-/*   Updated: 2021/11/26 13:39:50 by camillebarb      ###   ########.fr       */
+/*   Updated: 2021/11/28 12:30:51 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void    ft_lstclear(t_list **lst, void (*del)(void*))
+{
+   t_list   *list;
+
+   list = *lst;
+   while (list->next != NULL)
+   {
+       ft_lstdelone(list, del);
+       list = list->next;
+   }
+   return (NULL);
+}
