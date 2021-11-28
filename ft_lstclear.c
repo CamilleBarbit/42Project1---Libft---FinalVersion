d572b33605bf8713d092bea49d5560302b7efcf1
Camille Barbit <barbitcamille@gmail.com>
2021-11-28 12:45:36 +0100

    new version:need to check conditions

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 13:39:28 by camillebarb       #+#    #+#             */
-/*   Updated: 2021/11/28 12:33:03 by camillebarb      ###   ########.fr       */
+/*   Updated: 2021/11/28 12:44:57 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void    ft_lstclear(t_list **lst, void (*del)(void*))
    t_list   *list;
 
    list = *lst;
+   if (list->next == NULL)
+        ft_lstdelone(list, del);
    while (list->next != NULL)
    {
        ft_lstdelone(list, del);
