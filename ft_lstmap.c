6888b81fbd0256d8fd7a9e3d485a1a8097abd53b
Camille Barbit <barbitcamille@gmail.com>
2021-11-29 11:54:03 +0100

    new version

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 14:37:28 by camillebarb       #+#    #+#             */
-/*   Updated: 2021/11/28 15:19:49 by camillebarb      ###   ########.fr       */
+/*   Updated: 2021/11/29 11:53:25 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,67 @@
 t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
     t_list  *new;
-    t_list  *modif;
+    t_list  *modif_lst;
 
-    new = ft_lstnew(lst->content);
-    modif = new;
-    lst = lst->content;
-    while (lst->next != NULL)
+    new = ft_lstnew((*f)(lst->content));
+    modif_lst = new;
+    lst = lst->next;
+    while (lst != NULL)
     {
-        new = ft_lstnew(lst->content);
-        
-
+        if (!new)
+        {
+            ft_lstclear(&modif_lst, del);
+            return (0);
+        }  
+        new = new->next;
+        new = ft_lstnew((*f)(lst->content));
+        modif_lst->next = ft_lstadd_back(&modif_lst, new);
+        lst = lst->next;
     }
+    return (modif);
+}
 
+void    *foo(void *content)
+{
+    char    *newcontent;
 
+    newcontent = (char *)content;
+    newcontent = "oui";
+    return (newcontent);
+}
 
-    return (new);
+void    del(void *content)
+{
+    free(content);
+}
+
+void    afficher(t_list *maillon)
+{
+    while (maillon)
+    {
+        printf("%s\n", maillon->content);
+        maillon = maillon->next;
+    }
+}
+
+int main()
+{
+    t_list  *maillon1;
+    t_list  *maillon2;
+    t_list  *maillon3;
+    t_list  *new;
+
+    maillon1 = ft_lstnew("salut");
+    maillon2 = ft_lstnew("ça va");
+    maillon3 = ft_lstnew("oui, merci");
+    new = ft_lstnew(NULL);
+
+    maillon1->next = maillon2;
+    maillon2->next = maillon3;
+
+    afficher(maillon1);
+    new = ft_lstmap(maillon1, foo, del);
+    afficher(new);
+    return (0);
+
 }
